35fc6685e0d4fa72754fe9601fbc545817418bf8
Cris Shaki <cmshaki@gmail.com>
2021-10-25 12:31:01 +0300

    Adding unversioned files

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.criminalintent
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.core.os.bundleOf
+import androidx.fragment.app.*
+import java.util.*
+
+private const val ARG_DATE = "date"
+private const val REQUEST_DATE = "0"
+private const val RESULT_DATE = "resultDate"
+
+class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val date = arguments?.getSerializable(ARG_DATE) as Date
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        val initialYear = calendar.get(Calendar.YEAR)
+        val initialMonth = calendar.get(Calendar.MONTH)
+        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireContext(), this, initialYear, initialMonth, initialDay)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        val resultDate : Date = GregorianCalendar(year, month, day).time
+        setFragmentResult(REQUEST_DATE, bundleOf(RESULT_DATE to resultDate))
+    }
+
+    companion object {
+        fun newInstance(date: Date): DatePickerFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+
+            return DatePickerFragment().apply {
+                arguments = args
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.kt
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.criminalintent
+
+import android.app.Dialog
+import androidx.fragment.app.DialogFragment
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.widget.TimePicker
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import java.util.*
+
+private const val ARG_DATE = "date"
+private const val REQUEST_DATE = "0"
+private const val RESULT_DATE = "resultDate"
+
+class TimePickerFragment: DialogFragment(), TimePickerDialog.OnTimeSetListener {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current time as the default values for the picker
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        // Create a new instance of TimePickerDialog and return it
+        return TimePickerDialog(requireContext(), this, hour, minute, DateFormat.is24HourFormat(requireContext()))
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        // Do something with the time chosen by the user
+        val date = arguments?.get(ARG_DATE)
+        val calendar = Calendar.getInstance()
+        calendar.time = date as Date
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        calendar.set(Calendar.MINUTE, minute)
+        val resultDate = calendar.time
+        setFragmentResult(REQUEST_DATE, bundleOf(RESULT_DATE to resultDate))
+    }
+
+    companion object {
+        fun newInstance(date: Date): TimePickerFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+
+            return TimePickerFragment().apply {
+                arguments = args
+            }
+        }
+    }
+}
